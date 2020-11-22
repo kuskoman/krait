3bb47c5ade9a9f704d8f80430000e4a951938b2b
kkm <kubasurdej@gmail.com>
2020-11-22 16:54:06 +0100

    Rename emitter/index.ts to interfaceEmitter.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import { parseKraitConfig } from "./config";
+import { generateInterface } from "./emitters/interfaceEmitter";
+import { generateInterfaceTree } from "./parser";
 
 const cfg = parseKraitConfig();
 const treeNode = generateInterfaceTree(structure);
 const cfgInterfaceName = cfg.configInterfaceName || "Config";
-return generateInterface(cfgInterfaceName, treeNode);
+generateInterface(cfgInterfaceName, treeNode);
